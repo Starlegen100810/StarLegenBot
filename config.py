dcd2e1ae36a292f01aa1255374d4d826cb057f80
Hayk <babyangels.am7@gmail.com>
2025-08-12 22:10:21 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 BOT_TOKEN = "7198636747:AAEUNsaiMZXweWcLZoQcxocZKKLhxapCszM"
 ADMIN_ID = 6822052289
-Key: WEBHOOK_URL
-Value: https://babyangelsbot08.onrender.com
+admin_list = [6822052289]
+WEBHOOK_HOST = "" https://babyangelsbot08.onrender.com
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_URL  = WEBHOOK_HOST + WEBHOOK_PATH
+HOST = "0.0.0.0"
+PORT = 10000
+BASE_DIR   = os.path.dirname(os.path.abspath(__file__))
+MEDIA_DIR  = os.path.join(BASE_DIR, "products")
+BUNNY_IMG  = os.path.join(BASE_DIR, "bunny.jpg")
 
+DEFAULT_LANG = "hy"  # hy / ru / en
